c0e256aab92c020a7c5d30df30964700c2aacd41
yeawz <filatovw76@gmail.com>
2025-01-14 22:50:06 +0700

    исправлен отступ

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #define SIZE 1024
 int main(int argc, char** argv) {
  char* str = (char*)malloc(sizeof(char) * SIZE);
- printf("Введите своё имя:\n");
+ printf("Введите своё имя: \n");
  scanf("%s", str);
  printf("Привет, %s", str);
  return 0;
