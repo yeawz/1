81d5aae5741dea28b9c6133880cfe735a010f36d
yeawz <filatovw76@gmail.com>
2025-01-14 23:34:40 +0700

    изменения в файле

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define SIZE 1024
+
 int main(int argc, char** argv) {
- char* str = (char*)malloc(sizeof(char) * SIZE);
- printf("Введите своё имя:\n");
-str = gets(str);
- printf("Привет, %s", str);
- return 0;
+    char* str = (char*)malloc(sizeof(char) * SIZE);
+    printf("Введите своё имя: n");
+    fgets(str, SIZE, stdin);
+    printf("Привет, %sn", str);
+    free(str);  
+    return 0;
 }
